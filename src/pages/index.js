ea2518f02a463c8cdd4c78ab23d6ce9f6efebb01
Ty Day <tyrday@gmail.com>
2019-07-25 13:33:07 -0400

    commit test

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Welcome to my website</h1>
+    <h1>Header One Here</h1>
     <p>Sample site for the Gatsby crash course.</p>
     <p>Now go build something great.</p>
+    <br/>
+    <p>Followed along with Traversy Media tutorila</p>
+    <a href="https://www.youtube.com/watch?v=6YhqQ2ZW1sc">Link to course here</a>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
